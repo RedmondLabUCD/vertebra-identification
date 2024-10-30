876390bfc3decac7736895a83093f57aefc54e14
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2024-10-30 04:48:22 +0000

    fix array errors in eval metric

--- slurm_script.sh
+++ slurm_script.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=14G            ### How much RAM memory do you need?
 #SBATCH -p short           ### The queue to submit to: express, short, long, interactive
 #SBATCH --gres=gpu:1         ### How many GPUs do you need?
-#SBATCH -t 0-12:00:00        ### The time limit in D-hh:mm:ss format
+#SBATCH -t 0-48:00:00        ### The time limit in D-hh:mm:ss format
 #SBATCH -o /trinity/home/r094879/repositories/vertebra-identification/output/out_%j.log       ### Where to store the console output (%j is the job number)
 #SBATCH -e /trinity/home/r094879/repositories/vertebra-identification/error/error_%j.log      ### Where to store the error output
 #SBATCH --job-name=sp_lm_model  ### Name your job so you can distinguish between jobs
@@ -21,5 +21,5 @@ source /trinity/home/r094879/vertebra-detection/bin/activate
 
 # ----- Your tasks -----
 # python final_training.py UNet_LM_CL --custom_loss True
-# python main.py
-python test.py UNet_LM_CL
+python main.py
+# python test.py UNet_LM_CL

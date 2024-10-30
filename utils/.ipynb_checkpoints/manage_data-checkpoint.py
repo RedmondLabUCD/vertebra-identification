38ee9739bbab9e1c7c0e5ff762d48ad464db9f1d
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2024-10-30 06:09:07 +0000

    fix array errors in eval metric

--- utils/.ipynb_checkpoints/manage_data-checkpoint.py
+++ utils/.ipynb_checkpoints/manage_data-checkpoint.py
@@ -22,23 +22,27 @@ from utils.feature_extraction import extract_image_size
 
 def prep_data():
 
-    df = pd.DataFrame(columns = ['image','id','group','T4x','T4y','T5x','T5y','T6x','T6y','T7x','T7y','T8x','T8y','T9x','T9y',
-                             'T10x','T10y','T11x','T11y','T12x','T12y','L1x','L1y','L2x','L2y','L3x','L3y','L4x','L4y'])
+    # df = pd.DataFrame(columns = ['image','id','group','T4x','T4y','T5x','T5y','T6x','T6y','T7x','T7y','T8x','T8y','T9x','T9y',
+    #                          'T10x','T10y','T11x','T11y','T12x','T12y','L1x','L1y','L2x','L2y','L3x','L3y','L4x','L4y'])
 
-    filenames = [os.path.normpath(file).split(os.path.sep)[-1][:-4]
-                     for file in glob('//data/scratch/r094879/data/images/*.dcm')]
+    # filenames = [os.path.normpath(file).split(os.path.sep)[-1][:-4]
+    #                  for file in glob('//data/scratch/r094879/data/images/*.dcm')]
 
-    df['image'] = filenames
+    # df['image'] = filenames
 
-    # Read the Excel file
-    mappings_file = '//data/scratch/r094879/data/annotations/mappings.csv' 
-    df_x = pd.read_csv(mappings_file)
+    # # Read the Excel file
+    # mappings_file = '//data/scratch/r094879/data/annotations/mappings.csv' 
+    # df_x = pd.read_csv(mappings_file)
 
-    # Loop through each row in the Excel file and process
-    for index, row in df_x.iterrows():
-        create_data_file(row,df)
+    # # Loop through each row in the Excel file and process
+    # for index, row in df_x.iterrows():
+    #     create_data_file(row,df)
 
-    df2 = df.replace('', np.nan, regex=True)
+    # df2 = df.replace('', np.nan, regex=True)
+
+    annotations_file = '//data/scratch/r094879/data/annotations/annotations.csv' 
+    df2 = pd.read_csv(annotations_file)
+    
     imgs_to_delete = df2['image'][df2['group'].isna()].tolist()
 
     count = 0

9d4ec6adccc4815b3cec967f9d378c4eec385e2a
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2024-11-12 01:02:58 +0000

    added mask creation

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,4 +1,4 @@
-from utils.manage_data import prep_data, plot_images_with_points, create_dataset, view_heatmaps, plot_images_with_points_256
+from utils.manage_data import prep_data, plot_images_with_points, create_dataset, view_heatmaps, plot_images_with_points_256, gather_boundaries, create_mask
 
 
 if __name__ == '__main__':

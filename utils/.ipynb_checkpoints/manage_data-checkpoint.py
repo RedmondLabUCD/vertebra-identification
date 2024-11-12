eaa3150b7cc64e2ad9534a34300991fa2ff6b4d3
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2024-11-12 22:51:45 +0000

    mask creation

--- utils/.ipynb_checkpoints/manage_data-checkpoint.py
+++ utils/.ipynb_checkpoints/manage_data-checkpoint.py
@@ -183,15 +183,16 @@ def gather_boundaries(row):
 
             xy_pairs = []
             
-            for i in range(74):
-                num_x = 18010 + (2*i)
-                num_y = 18010 + (2*i) + 1
+            for i in range(78):
+                num_x = 18002 + (2*i)
+                num_y = 18002 + (2*i) + 1
                 if int(num_x) >= 18100:
                     num_x = num_x + 16
                     num_y = num_y + 16
                 bx = spss_row[variable_names[group]+'_'+str(num_x)+'.'+str(vertebra)].values[0]
                 by = spss_row[variable_names[group]+'_'+str(num_y)+'.'+str(vertebra)].values[0]
                 xy_pairs.append([int(bx),int(by)])
+            print(xy_pairs)
             
             if len(xy_pairs) != 0:   
                 create_mask(image_name,xy_pairs)

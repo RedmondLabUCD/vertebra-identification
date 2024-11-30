34e9e04c742fa4fb0ae2be949f66def9c742bd5f
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2024-11-30 09:51:52 +0000

    new loss function

--- slurm_script.sh
+++ slurm_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --ntasks=1           ### How many CPU cores do you need?
 #SBATCH --mem=14G            ### How much RAM memory do you need?
-#SBATCH -p short           ### The queue to submit to: express, short, long, interactive
+#SBATCH -p long           ### The queue to submit to: express, short, long, interactive
 #SBATCH --gres=gpu:1         ### How many GPUs do you need?
-#SBATCH -t 0-10:00:00        ### The time limit in D-hh:mm:ss format
+#SBATCH -t 6-00:00:00        ### The time limit in D-hh:mm:ss format
 #SBATCH -o /trinity/home/r094879/repositories/vertebra-identification/output/out_%j.log       ### Where to store the console output (%j is the job number)
 #SBATCH -e /trinity/home/r094879/repositories/vertebra-identification/error/error_%j.log      ### Where to store the error output
 #SBATCH --job-name=sp_lm_model  ### Name your job so you can distinguish between jobs
@@ -21,7 +21,7 @@ source /trinity/home/r094879/vertebra-detection/bin/activate
 
 # ----- Your tasks -----
 python final_training.py UNet_LM_CL --custom_loss True --ckpt 'Checkpoint/Long'
-python final_training.py UNet_LM_CL3 --custom_loss True --ckpt 'Checkpoint/Long3'
+# python final_training.py UNet_LM_CL3 --custom_loss True --ckpt 'Checkpoint/Long3'
 # python main.py
 # python test.py UNet_LM_CL --ckpt 'Checkpoint/Long'
 # python test.py UNet_LM_CL --ckpt 'Checkpoint/Long'

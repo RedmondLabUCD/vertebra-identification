9c40a3adc60e2cd7aa3c6906a050f1fe28b02629
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2024-12-19 01:17:33 +0000

    Added landmark saving csv to test

--- slurm_script_2.sh
+++ slurm_script_2.sh
@@ -21,4 +21,7 @@ source /trinity/home/r094879/vertebra-detection/bin/activate
 
 # ----- Your tasks -----
 # python final_training.py UNet_LM_CL2 --custom_loss True
-python final_training.py UNet_deep_CL2 --custom_loss True
+# python final_training.py UNet_deep_CL2 --custom_loss True
+python test.py UNet_LM_CL
+python test.py UNet_LM_CL2
+python test.py UNet_LM_CL3

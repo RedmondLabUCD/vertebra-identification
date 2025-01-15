0ce16169c6482d6c399ccae6f20e88900924886b
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2025-01-15 14:00:35 +0000

    change plot point format

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     # create_dataset()
     # plot_images_with_points()
     # prep_data()
-    plot_test_images_with_points('UNet_LM_CL_2.csv',name='LM2')
-    plot_test_images_with_points('UNet_LM_CL_3.csv',name='LM3')
+    plot_test_images_with_points('UNet_LM_CL2.csv',name='LM2')
+    plot_test_images_with_points('UNet_LM_CL3.csv',name='LM3')

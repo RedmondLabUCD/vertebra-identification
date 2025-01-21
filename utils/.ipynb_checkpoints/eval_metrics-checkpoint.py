0ce75a9c64010c61653e3bb698339130dd723714
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2025-01-21 20:32:28 +0000

    add identification accuracy check

--- utils/.ipynb_checkpoints/eval_metrics-checkpoint.py
+++ utils/.ipynb_checkpoints/eval_metrics-checkpoint.py
@@ -308,8 +308,8 @@ class pb_mse_metric_test(nn.Module):
         count = 0
         dist = (lm_targets[1,1]-lm_targets[0,1])/2
         for i in range(len(lm_targets)):
-            if abs(lm_preds[i,1]-lm_targets[i,1]) < dist:
-                if abs(lm_preds[i,0]-lm_targets[i,0]) < dist:
+            if abs(int(lm_preds[i,1])-int(lm_targets[i,1])) < dist:
+                if abs(int(lm_preds[i,0])-int(lm_targets[i,0])) < dist:
                     count+=1
         id_acc = count/len(lm_targets)
         

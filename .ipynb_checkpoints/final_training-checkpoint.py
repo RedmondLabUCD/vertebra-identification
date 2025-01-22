848db4b2d85cfb37f06198dae1cdde9b4cdf0435
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2025-01-22 14:28:59 +0000

    add identification accuracy check

--- .ipynb_checkpoints/final_training-checkpoint.py
+++ .ipynb_checkpoints/final_training-checkpoint.py
@@ -162,7 +162,7 @@ def main():
     # ==================== TRAIN THE MODEL FOR ONE FOLD ====================
 
     epoch_max = 100
-    for epoch in range(1,epoch_max):
+    for epoch in range(3,epoch_max):
         print("Epoch: {}".format(epoch))
         # Call training function. 
         train_generator = train(model, train_loader, optimizer, criterion, params)

58946c9a3f42153091fcad12fffeb2403acc983b
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2025-01-27 14:45:15 +0000

    edit data split

--- utils/manage_data.py
+++ utils/manage_data.py
@@ -137,9 +137,7 @@ def split_data_for_check():
     output_dir = output_dir1
     listing = glob(os.path.join(orig_dir,'*.png'))
     for name in listing:
-        filename = name.split('/')[-1]
-        print(filename)
-        print(os.path.join(output_dir,filename))
+        filename = name.split('//')[-1]
         shutil.move(os.path.join(orig_dir,filename),os.path.join(output_dir,filename))
         count += 1
         if count > 1800:
